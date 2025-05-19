acbc2e20fd8c1b23135741c039fc05578e3ad437
Anna Ingmer Boye <151528922+annaib00@users.noreply.github.com>
2025-05-19 15:32:31 +0200

    Add noise model, missing changes so VSLsolver

---
+++ noise_model.py
@@ -0,0 +1,25 @@
+import pennylane as qml
+import numpy as np
+
+@qml.BooleanFn
+def rot_cond(op):
+    return isinstance(op, qml.Rot) 
+
+depol_error = qml.noise.partial_wires(qml.DepolarizingChannel, 0.0)
+
+fcond_rot, noise_rot = rot_cond, depol_error
+
+@qml.BooleanFn
+def CZ_cond(op):
+    return isinstance(op, qml.CZ) 
+
+depol_error = qml.noise.partial_wires(qml.DepolarizingChannel, 0.0)
+
+fcond_rot, noise_rot = CZ_cond, depol_error
+
+
+
+
+noise_model = qml.NoiseModel(
+    {fcond_rot: noise_rot, CZ_cond: depol_error}, 
+)
