6c5147af5bf88214fd3aba1ffef260a69da9fe1a
llwcybwy <colpier@chalmers.se>
2025-06-03 18:05:42 +0200

    Added poisson matrix creator for a given size n

--- conditioned_matrix.py
+++ conditioned_matrix.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.sparse as sp
+from tqdm import tqdm
 
 def create_conditioned_random_matrix(qubits: int, condition_number: float):
     """
@@ -23,6 +25,25 @@ def create_conditioned_random_matrix(qubits: int, condition_number: float):
     m_conditioned = U @ S_conditioned @ Vh
     return m_conditioned
 
+def create_poisson_matrix(n: int):
+    """
+    Generates the discrete Poisson equation matrix for an n x n grid (2D Laplacian with Dirichlet boundary conditions).
+    :param n: Number of grid points in one dimension.
+    :return: (n*n, n*n) scipy.sparse.csr_matrix representing the Poisson matrix.
+    """
+    N = n * n
+    main_diag = 4 * np.ones(N)
+    side_diag = -1 * np.ones(N - 1)
+    side_diag[np.arange(1, N) % n == 0] = 0  # zero out wrap-around connections
+    up_down_diag = -1 * np.ones(N - n)
+
+    diagonals = [main_diag, side_diag, side_diag, up_down_diag, up_down_diag]
+    offsets = [0, -1, 1, -n, n]
+    A = sp.diags(diagonals, offsets, shape=(N, N), format='csr')
+    return A
+
 if __name__ == "__main__":
     m = create_conditioned_random_matrix(100, 1000)
-    print(f"condition number is {np.linalg.cond(m)}")
+    print(f"condition number for dense is {np.linalg.cond(m)}")
+    m_p = create_poisson_matrix(2**9)
+    # print(f"condition number for Poisson is {np.linalg.cond(m_p)}")
