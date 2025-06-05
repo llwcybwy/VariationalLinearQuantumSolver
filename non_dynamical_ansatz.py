10c3fd5c27fb4ed915d6b5a055d0fad7998a124f
Anna Ingmer Boye <151528922+annaib00@users.noreply.github.com>
2025-06-05 11:22:07 +0200

    Add non dynamical ansatz

---
+++ non_dynamical_ansatz.py
@@ -0,0 +1,30 @@
+import pennylane as qml
+
+def fixed_layered_ansatz(params):
+    wires = range(3)  
+    depth = 10
+    n_qubits = len(wires)
+    param_idx = 0
+
+    for i in range(n_qubits):
+            qml.RY(params[param_idx], wires=wires[i])
+            param_idx += 1
+
+    for d in range(depth):
+        # CZ gates on alternating pairs 
+        for i in range(0, n_qubits - 1, 2):
+            qml.CZ(wires=[wires[i], wires[i + 1]])
+            
+        # RY rotations on all 
+        for i in range(n_qubits):
+            qml.RY(params[param_idx], wires=wires[i])
+            param_idx += 1
+
+        # CZ gates on alternating pairs (odd pairs)
+        for i in range(1, n_qubits - 2, 2):
+            qml.CZ(wires=[wires[i], wires[i + 1]])
+
+        # RY rotations on all qubits, except the first and last one
+        for i in range(1, n_qubits-1):
+            qml.RY(params[param_idx], wires=wires[i])
+            param_idx += 1
